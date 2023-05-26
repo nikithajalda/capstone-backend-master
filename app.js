16672ead1a713509746b0a7b3572fda03416467a
Nikitha <nikithajalda@gmail.com>
2023-05-27 01:53:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require('./index');
+const PORT = 3001;
+
+const connectionString =
+  "mongodb+srv://newUser:1234@cluster0.krzyobz.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+
+const corsOptions = {
+  exposedHeaders: ["x-auth-token", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', routes);
+
+mongoose.connect(connectionString, {useNewUrlParser:true,useUnifiedTopology:true})
+  .then(() => app.listen(PORT, () => console.log(`server is running at PORT : ${PORT}`)))
+  .catch((err) => console.log(err.message));
